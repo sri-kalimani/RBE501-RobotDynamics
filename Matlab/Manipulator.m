edeb7ae44829144d1f51ab2aae41816cdcad578c
kaboenisch <42717655+kaboenisch@users.noreply.github.com>
2021-05-11 15:12:15 -0400

    Update Manipulator.m
    Fixed DH and screw axes, fwd kin working

--- Matlab/Manipulator.m
+++ Matlab/Manipulator.m
@@ -11,22 +11,22 @@ nTests = 20; % number of random test configurations
 
 %% Create the manipulator
 % robot length values (meters)
-L1 = 0.0465;
-L2 = 0.117;
-L3 = 0.14959;
-L4 = 0.03601;
+L1 = 0.095;
+L2 = 0.120;
+L3 = 0.150;
+L4 = 0.0356;
 
-robot = SerialLink([Revolute('a', 0, 'd', L1, 'alpha',0), ...
-                    Revolute('a', L2, 'd', 0, 'alpha',0), ...
+robot = SerialLink([Revolute('a', 0, 'd', L1, 'alpha',-pi/2), ...
+                    Revolute('a', L2, 'd', 0, 'alpha',0, 'offset', -pi/2), ...
                     Revolute('a', 0, 'd', 0, 'alpha', -pi/2, 'offset', 0), ...
                     Revolute('a', 0, 'd', L3, 'alpha', pi/2, 'offset', 0), ...
                     Revolute('a', 0, 'd', 0, 'alpha', -pi/2, 'offset', 0), ...
-                    Revolute('a', 0, 'd', L4, 'alpha', 0, 'offset', 0)], 'name', 'Fanuc LR Mate 200iD'); 
+                    Revolute('a', 0, 'd', L4, 'alpha', 0, 'offset', 0)], 'name', 'RBE501 Lab Robot'); 
 
 % Joint limits
-qlim = [-pi  pi;        % q(1)
+qlim = [-pi/2  pi/2;      % q(1)
         -pi/2  pi/2;  % q(2)
-        -pi/2  pi/2;  % q(3)              %FIX LIMITS
+        -3*pi/4  pi/4;  % q(3)              %FIX LIMITS
         -pi/2  pi/2;  % q(4)
         -pi/2  pi/2;  % q(5)
         -pi/2  pi/2]; % q(6)
@@ -35,28 +35,20 @@ qlim = [-pi  pi;        % q(1)
 q = zeros(1,6);
 robot.teach(q);
 %% Part A - Calculate the screw axes
-% w1 = [0; 0; 1];
-% w2 = [0; 1; 0];
-% w3 = [0; -1; 0];
-% w4 = [1; 0; 0];
-% w5 = [0; 1; 0];
-% w6 = [0; 0; 1];
-
 w1 = [0; 0; 1];
 w2 = [0; 1; 0];
-w3 = [0; -1; 0];
+w3 = [0; 1; 0];
 w4 = [1; 0; 0];
-w5 = [0; -1; 0];
+w5 = [0; 1; 0];
 w6 = [1; 0; 0];
 
 p1 = [0; 0; 0];
 p2 = [0; 0; L1];
 p3 = [0; 0; L2+L1];
-p4 = p3;
+p4 = [L3; 0; L2+L1];
 p5 = [L3; 0; L2+L1];
-p6 = p5;
+p6 = [L3; 0; L2+L1];
 p7 = [L3+L4; 0; L2+L1]; 
-% p6 = [L3+L4; -W; L1+L2];
 
 v1 = cross(-w1, p1);
 v2 = cross(-w2, p2);
@@ -66,17 +58,14 @@ v5 = cross(-w5, p5);
 v6 = cross(-w6, p6);
 
 S = [w1 w2 w3 w4 w5 w6;
-    v1 v2 v3 v4 v5 v6;];
+     v1 v2 v3 v4 v5 v6;];
 
-
-% S = [ w1 w2 % w3 w4 w5 w6;
-%       v1 v2];% v3 v4 v5 v6;];
- %% Part B - Calculate the forward kinematics with the Product of Exponentials formula
+%% Part B - Calculate the forward kinematics with the Product of Exponentials formula
 % First, let us calculate the homogeneous transformation matrix M for the
 % home configuration
 
 R = [0 0 1;
-     0 1 0;
+     0 -1 0;
      1 0 0];
 M = [R p7;
      0 0 0 1];
@@ -101,7 +90,7 @@ for ii = 1 : nTests
     
     % Calculate the forward kinematics
         T = fkine(S, M, q)
-%       T = robot.fkine(q)
+       %T = robot.fkine(q);
     
     if plotOn
         robot.teach(q);
@@ -110,7 +99,7 @@ for ii = 1 : nTests
     
     %For testing
     T_real = robot.fkine(q)
-    T_diff = T - double(T_real)
+    T_diff = T - double(T_real);
     assert(all(all(abs(double(robot.fkine(q)) - T) < 1e-10)));
 end
  
