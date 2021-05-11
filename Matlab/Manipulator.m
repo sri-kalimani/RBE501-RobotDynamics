920e4dafc64ef1bd173407eba11f5226773d68a5
Nlkuberka <nlkuberka@wpi.edu>
2021-05-11 15:10:19 -0400

    Update Manipulator.m
    Final completed

--- Matlab/Manipulator.m
+++ Matlab/Manipulator.m
@@ -11,37 +11,30 @@ nTests = 20; % number of random test configurations
 
 %% Create the manipulator
 % robot length values (meters)
-L1 = 0.0465;
-L2 = 0.117;
-L3 = 0.14959;
-L4 = 0.03601;
+L1 = 0.095;
+L2 = 0.120;
+L3 = 0.150;
+L4 = 0.0356;
 
 robot = SerialLink([Revolute('a', 0, 'd', L1, 'alpha',-pi/2), ...
-                    Revolute('a', L2, 'd', 0, 'alpha',0), ...
+                    Revolute('a', L2, 'd', 0, 'alpha',0, 'offset', -pi/2), ...
                     Revolute('a', 0, 'd', 0, 'alpha', -pi/2, 'offset', 0), ...
                     Revolute('a', 0, 'd', L3, 'alpha', pi/2, 'offset', 0), ...
                     Revolute('a', 0, 'd', 0, 'alpha', -pi/2, 'offset', 0), ...
-                    Revolute('a', 0, 'd', L4, 'alpha', 0, 'offset', 0)], 'name', 'Fanuc LR Mate 200iD'); 
+                    Revolute('a', 0, 'd', L4, 'alpha', 0, 'offset', 0)], 'name', 'RBE501 Lab Robot'); 
 
 % Joint limits
-qlim = [-pi  pi;      % q(1)
-        -pi  pi;  % q(2)
-        -pi  pi;  % q(3)              %FIX LIMITS
-        -pi  pi;  % q(4)
-        -pi  pi;  % q(5)
-        -pi  pi]; % q(6)
+qlim = [-pi/2  pi/2;      % q(1)
+        -pi/2  pi/2;  % q(2)
+        -3*pi/4  pi/4;  % q(3)              %FIX LIMITS
+        -pi/2  pi/2;  % q(4)
+        -pi/2  pi/2;  % q(5)
+        -pi/2  pi/2]; % q(6)
 
 % Display the manipulator in the home configuration
 q = zeros(1,6);
 robot.teach(q);
 %% Part A - Calculate the screw axes
-% w1 = [0; 0; 1];
-% w2 = [0; 1; 0];
-% w3 = [0; -1; 0];
-% w4 = [1; 0; 0];
-% w5 = [0; 1; 0];
-% w6 = [0; 0; 1];
-
 w1 = [0; 0; 1];
 w2 = [0; 1; 0];
 w3 = [0; 1; 0];
@@ -56,7 +49,6 @@ p4 = [L3; 0; L2+L1];
 p5 = [L3; 0; L2+L1];
 p6 = [L3; 0; L2+L1];
 p7 = [L3+L4; 0; L2+L1]; 
-% p6 = [L3+L4; -W; L1+L2];
 
 v1 = cross(-w1, p1);
 v2 = cross(-w2, p2);
@@ -68,15 +60,12 @@ v6 = cross(-w6, p6);
 S = [w1 w2 w3 w4 w5 w6;
      v1 v2 v3 v4 v5 v6;];
 
-
-% S = [ w1 w2 % w3 w4 w5 w6;
-%       v1 v2];% v3 v4 v5 v6;];
- %% Part B - Calculate the forward kinematics with the Product of Exponentials formula
+%% Part B - Calculate the forward kinematics with the Product of Exponentials formula
 % First, let us calculate the homogeneous transformation matrix M for the
 % home configuration
 
 R = [0 0 1;
-     0 1 0;
+     0 -1 0;
      1 0 0];
 M = [R p7;
      0 0 0 1];
